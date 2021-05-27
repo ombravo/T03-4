1dd9dbeb5d4f7a57661493d002f3169e959c562c
OSCAR MANUEL BRAVO PRIETO <83453870+ombravo@users.noreply.github.com>
2021-05-27 08:15:56 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T03_4
+{
+    class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            int N = 1;
+        int resultado = 0;
+            Console.WriteLine("Valor inicial de N es {0}", N);
+
+            resultado = N + 77;
+            Console.WriteLine("N + 77 = {0}", resultado);
+
+            resultado = resultado - 3;
+            Console.WriteLine("N - 3 = {0}", resultado);
+
+            resultado = resultado * 2;
+            Console.WriteLine("N * 2 = {0}", resultado);
+        }
+    }
+}
